8a6f5c07036461c2be3af0ee5a60059e56f4b3f8
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2025-01-06 20:58:18 +1300

    damn same bug appeared

--- main.py
+++ main.py
@@ -7,17 +7,20 @@ bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 # What to do when bot is ready
 @bot.event
 async def on_ready():
-    print("Ready!")
-
     try:
         synced_commands = await bot.tree.sync()
         print(f"Synced {len(synced_commands)} commands")
     except Exception as e:
         print(f"An error occured: {e}")
 
+# Hello command
 @bot.tree.command(name="hello", description="Greets you with a hello!")
 async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Hello {interaction.user.mention}")
+    await interaction.response.send_message(f"Hello {interaction.user.mention}!")
+
+@bot.tree.command(name="print", description="Sends the message that you entered")
+async def print(interaction: discord.Interaction, printer: str):
+    await interaction.response.send_message(printer)
 
 # Environment variables
 with open("token.txt") as token_file:
