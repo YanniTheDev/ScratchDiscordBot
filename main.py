60e14fe270b3b2ccca163996ddf1358a73cffab0
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2025-01-06 19:42:24 +1300

    did a safety handle

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ class Client(discord.Client):
         print(f"Logged on as {self.user}")
 
     async def on_message(self, message):
-        print(f"Message from {message.author}: {message.content}")
+        # Checks to see if the bot has sent a message by itself
+        if message.author == self.user:
+            return
 
 intents = discord.Intents.default()
 intents.message_content = True
