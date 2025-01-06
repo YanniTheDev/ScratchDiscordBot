7cc81fb5b99aac4db0579d0b623198e40d281e85
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2025-01-06 20:27:38 +1300

    alrighty shoul work now

--- main.py
+++ main.py
@@ -5,7 +5,22 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
-bot = 
+# Initialize the bot
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+
+# What to do when bot is ready
+@bot.event
+async def on_ready(self):
+    print(f"Ready as {self.user}...")
+
+'''
+@param ctx: 
+ctx stands for context. It stores all the info about the command that was executed
+such as who executed it, the server that was executed in, etc
+'''
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
 
 # Environment variables
 bot_token = os.getenv("BOT_TOKEN")
