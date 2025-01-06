b86858683da5f51d8d068b976b68f2b085b6ae21
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2025-01-06 20:21:35 +1300

    damn bugs need to restart

--- main.py
+++ main.py
@@ -3,34 +3,11 @@ import os
 from dotenv import load_dotenv
 
 import discord
-from discord import app_commands
 from discord.ext import commands
 
-load_dotenv()
-
-class Client(commands.Bot):
-    async def on_ready(self):
-        print(f"Logged on as {self.user}")
-
-    async def on_message(self, message):
-        # Checks to see if the bot has sent a message by itself
-        if message.author == self.user:
-            return
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = Client(command_prefix="?", intents=intents)
-
-@client.tree.command(name="hi", description="Say hello!")
-async def say_hello(interaction: discord.Interaction):
-    await interaction.response.send_message("Hi there!")
-
-@client.tree.command(name="print", description="Replies with a message")
-async def print(interaction: discord.Interaction, message: str):
-    await interaction.response.send_message(message)
+bot = 
 
 # Environment variables
 bot_token = os.getenv("BOT_TOKEN")
 
-client.run(bot_token)
+bot.run(bot_token)
